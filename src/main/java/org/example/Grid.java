f4220e1f7787953af1e4a996d122a1f8c4447d05
DrPferdemeister <drpferdemeister@outlook.de>
2023-02-23 17:48:22 +0100

    test

--- src/main/java/org/example/Grid.java
+++ src/main/java/org/example/Grid.java
@@ -1,2 +1,52 @@
-package org.example;public class Grid {
+package org.example;
+
+import java.util.Scanner;
+
+public class Grid {
+    char [][]grid = new char[6][7]; // ready
+    char player;
+
+    boolean checkIfWin(){
+
+
+
+
+        if (player == 'x'){
+            player ='o';
+        }else {
+            player ='x';
+        }
+        return false;
+    }
+    void play(char player){
+        Scanner scanner = new Scanner(System.in);
+        int selection;
+
+    while (!checkIfWin()){         // vllt in die Main
+        System.out.println("Spieler " + player +" ist dran! Wählen Sie die Spalte:");
+
+        selection = scanner.nextInt();
+        place(player, selection);
+        printGrid();
+        //checkIfWin();
+
+    }
+    }
+    void place(char player, int selection){
+        boolean isFree = false;
+        int j = 0;
+        for (int i = grid.length; '\0' == grid[i][selection]; i--) {
+            j = i;
+        }
+        grid[j][selection] = player;
+    }
+    void printGrid(){
+        for (int i = 0; i< grid.length; i++){
+            for (int j = 0; j< grid[i].length;j++){
+                System.out.print("|("+ grid[i][j] + ")" );
+            }
+            System.out.println("|");
+        }
+    }
+
 }
